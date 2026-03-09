ce3417d1dbae7316f37bbee322fc558a1ce3c85f
jhehaul-debug <jhehaul@gmail.com>
2026-03-09 11:34:41 +0000

    Import Job model from models.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import logging
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
-
 from sqlalchemy import Column, Integer, String, Text
+from models import Job
 from werkzeug.security import generate_password_hash, check_password_hash 
 logging.basicConfig(level=logging.INFO)
 from flask_login import LoginManager, UserMixin, login_required, current_user, login_user, logout_user
@@ -26,6 +26,9 @@ app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
+@login_manager.user_loader
+def load_user(user_id):
+    return db.session.get(User, int(user_id))
 # Secret key
 app.config["SECRET_KEY"] = os.environ.get("SESSION_SECRET", "dev-secret")
 
